facf3522d996f32906c183ceede89bd79c7f43fb
Dawson Brown <dawsonstbrown@gmail.com>
2024-05-02 16:50:34 -0400

    changes

--- src/search/search_algorithms/sparce_mcts.cc
+++ src/search/search_algorithms/sparce_mcts.cc
@@ -5,6 +5,7 @@
 #include "../open_list_factory.h"
 #include "../pruning_method.h"
 
+#include "../task_utils/task_properties.h"
 #include "../algorithms/ordered_set.h"
 #include "../plugins/options.h"
 #include "../task_utils/successor_generator.h"
@@ -27,6 +28,7 @@ SparseMCTS::SparseMCTS(const plugins::Options &opts)
       rng(utils::parse_rng_from_options(opts)),
       c(opts.get<double>("c")),
       epsilon(opts.get<double>("epsilon"))  {}
+
 
 bool SparseMCTS::is_dead_end(
     EvaluationContext &eval_context) const {
@@ -53,7 +55,18 @@ void SparseMCTS::initialize() {
         SearchNode node = search_space.get_node(initial_state);
         node.open_initial();
 
-        // TODO: set root
+        // shared_ptr<Node> node(new Node(
+        //     initial_state.get_id(),
+        //     OperatorID::no_operator,
+        //     nullptr,
+        //     eval_context.get_evaluator_value(heuristic.get())
+        // ));
+        root.reset(new Node(
+            initial_state.get_id(),
+            OperatorID::no_operator,
+            nullptr,
+            eval_context.get_evaluator_value(heuristic.get())
+        ));
 
     }
 
@@ -88,13 +101,59 @@ shared_ptr<SparseMCTS::Node> SparseMCTS::select(shared_ptr<SparseMCTS::Node> nod
     return best; // will return *node* if none of children are better
 }
 
-SparseMCTS::Outcome SparseMCTS::simulate(SparseMCTS::Node &node) {
+SparseMCTS::Outcome SparseMCTS::simulate(SparseMCTS::Node &node, vector<OperatorID> &path) {
     StateID start_id = node.id;
     Outcome oc;
 
+    State parent_s = state_registry.lookup_state(start_id);
     StateRegistry rollout_registry(task_proxy);
-    // do rollout like other branch...
+    State curr_state = parent_s;
+    vector<OperatorID> applicable_ops;
+    successor_generator.generate_applicable_ops(parent_s, applicable_ops);
 
+    node.set_rollout_length();
+    int rollout_limit = node.rl_length;
+    do {
+        OperatorID op_id = *rng->choose(applicable_ops);
+        OperatorProxy op = task_proxy.get_operators()[op_id];
+        curr_state = rollout_registry.get_successor_state(parent_s, op);
+        // curr_state = parent_s.get_unregistered_successor(op);
+        
+        applicable_ops.clear();
+        successor_generator.generate_applicable_ops(curr_state, applicable_ops);
+        if (applicable_ops.size() == 0) {
+            return Outcome(DEADEND);
+        }
+
+        path.push_back(op_id);
+        if (task_properties::is_goal_state(task_proxy, curr_state)) {
+            cout << "!!! Goal found: random_rollout !!!" << endl;
+
+            // for (auto op_id : curr_rollout_path) {
+            //     cout << task_proxy.get_operators()[op_id].get_name() << endl << endl;
+            // }
+            return Outcome(GOAL, 0); // end of recursion: success
+        }
+        parent_s = curr_state;
+
+        rollout_limit-=1;
+    } while(rollout_limit > 0);
+
+    EvaluationContext succ_eval_context(
+            curr_state, &statistics, false);
+    int eval = succ_eval_context.get_evaluator_value_or_infinity(heuristic.get());
+    if (eval == EvaluationResult::INFTY) {
+        return Outcome(DEADEND);
+    }
+
+    if (eval < node.h) { // if the rollout found an improvement
+        // return make_pair(HI, curr_rollout_path);
+        return Outcome(HI, eval);
+
+    } else {
+        return Outcome(UHR, eval);
+
+    }
 
 }
 
@@ -103,6 +162,12 @@ void SparseMCTS::back_propogate(Result result, SparseMCTS::Node &node) {
 
     float score = 1 ? result == HI : 0;
     node.update(score);
+    
+    if (result == HI) {
+        node.rollout_step = node.rl_length;
+    } else {
+        node.rollout_step+=1;
+    }
 
     std::shared_ptr<Node> current = node.parent;
     while (current) {
@@ -115,7 +180,9 @@ SearchStatus SparseMCTS::step()
 {
     shared_ptr<Node> selected = root;
     while (selected != select(selected)) {}
-    Outcome oc = simulate(*selected);
+
+    vector<OperatorID> rollout_path;
+    Outcome oc = simulate(*selected, rollout_path);
 
     if (oc.result == GOAL) {
 
@@ -125,21 +192,52 @@ SearchStatus SparseMCTS::step()
     if (oc.result != DEADEND) {
          if (oc.result == HI) {
             cached_select = selected;
-                // TODO: add oc.terminal to tree
-                // -- this involves opening path in state registry
-                // and including the Node into the child list and all that.
+            open_path_to_new_node(selected, rollout_path, oc);
+            // add_mcts_node(*selected, new_node);
          } else {
             cached_select = nullptr;
             if (rng->random() >= epsilon) {
-                // TODO: add oc.terminal to tree
-                // -- this involves opening path in state registry
-                // and including the Node into the child list and all that.
+                open_path_to_new_node(selected, rollout_path, oc);
+                // add_mcts_node(*selected, new_node);
             }
         }
     }
     back_propogate(oc.result, *selected);
     return IN_PROGRESS;
 }
+
+void SparseMCTS::open_path_to_new_node(shared_ptr<Node> selected, std::vector<OperatorID> path, Outcome oc) {
+    State state = state_registry.lookup_state(selected->id);
+    OperatorID last_op(OperatorID::no_operator);
+    for (OperatorID op_id : path) {
+        OperatorProxy op = task_proxy.get_operators()[op_id];
+        State succ_state = state_registry.get_successor_state(state, op);
+
+        SearchNode node = search_space.get_node(state);
+        SearchNode succ_node = search_space.get_node(succ_state);
+
+        if (succ_node.is_new()) {
+            succ_node.open(node, op, get_adjusted_cost(op));
+        } else if (succ_node.get_g() > node.get_g() + get_adjusted_cost(op)) {
+            succ_node.update_parent(node, op, get_adjusted_cost(op));
+        }
+
+        state = succ_state;
+        last_op = op_id;
+    }
+
+    shared_ptr<Node> node(new Node(
+        state.get_id(),
+        last_op,
+        selected,
+        oc.h
+    ));
+    selected->add_child(node);
+}
+
+// void SparseMCTS::add_mcts_node(Node& selected, shared_ptr<Node> new_node) {
+
+// }
 
 void SparseMCTS::dump_search_space() const {
     search_space.dump(task_proxy);
--- src/search/search_algorithms/sparse_mcts.h
+++ src/search/search_algorithms/sparse_mcts.h
@@ -1,12 +1,12 @@
 #ifndef SEARCH_ALGORITHMS_SPARSE_MCTS_H
 #define SEARCH_ALGORITHMS_SPARSE_MCTS_H
 
-#include "../open_list.h"
 #include "../search_algorithm.h"
 #include "../utils/rng.h"
 
 #include <memory>
 #include <vector>
+#include <random>
 
 class Evaluator;
 
@@ -24,16 +24,20 @@ class SparseMCTS : public SearchAlgorithm {
     double epsilon;
 
 protected:
+
     struct Node {
         StateID id;
         OperatorID op_id;
         std::shared_ptr<Node> parent;
         std::vector<std::shared_ptr<Node>> children;
+        int h = std::numeric_limits<int>::max();
         int num_visits = 0;
         float score = 0.0F;
+        int rollout_step = 0;
+        int rl_length = 0;
 
-        Node(StateID id, OperatorID op_id, std::shared_ptr<Node> parent)
-            : id(id), op_id(op_id), parent(parent) {}
+        Node(StateID id, OperatorID op_id, std::shared_ptr<Node> parent, int h)
+            : id(id), op_id(op_id), parent(parent), h(h) {}
 
         void update(float score) {
             this->score += score;
@@ -47,27 +51,39 @@ protected:
         double get_avg_score() {
             return score / num_visits;
         }
+
+        void set_rollout_length() {
+            int floor_log = std::floor(logb(rollout_step));
+            int range = rollout_step - floor_log + 1;
+            int num = std::rand() % range + floor_log;
+            rl_length = std::max(num, 0);
+        }
     };
-    shared_ptr<Node> root;
+    std::shared_ptr<Node> root;
 
     bool is_dead_end(EvaluationContext &eval_context) const;
     virtual void initialize() override;
 
-    shared_ptr<Node> cached_select = nullptr;
+    std::shared_ptr<Node> cached_select = nullptr;
     enum Result { DEADEND = -1, UHR, HI, GOAL };
     struct Outcome {
         Result result;
-        shared_ptr<Node> terminal;
+        int h;
 
-        Outcome(Result result, shared_ptr<Node> node) :
-            result(result), terminal(node) {}
+        Outcome(Result result, int h) :
+            result(result), h(h) {}
+        Outcome(Result result) :
+            result(result), h(std::numeric_limits<int>::max()) {}
         Outcome() :
-            result(UHR), terminal(nullptr) {}
+            result(UHR), h(std::numeric_limits<int>::max()) {}
     };
-    shared_ptr<SparseMCTS::Node> select(shared_ptr<SparseMCTS::Node> node);
-    Outcome simulate(SparseMCTS::Node &node);
+    std::shared_ptr<SparseMCTS::Node> select(std::shared_ptr<SparseMCTS::Node> node);
+    Outcome simulate(SparseMCTS::Node &node, std::vector<OperatorID> &path);
     void back_propogate(Result result, SparseMCTS::Node &node);
     virtual SearchStatus step() override;
+
+    void open_path_to_new_node(std::shared_ptr<Node> selected, std::vector<OperatorID> path, Outcome oc);
+    // void add_mcts_node(Node& selected, shared_ptr<Node> new_node);
 
 public:
     explicit SparseMCTS(const plugins::Options &opts);
@@ -78,7 +94,7 @@ public:
     void dump_search_space() const;
 };
 
-extern void add_options_to_feature(plugins::Feature &feature);
+extern void add_options_to_feature(plugins::Feature &feature); 
 }
 
 #endif
